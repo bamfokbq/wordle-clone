3f68982fd0c751ca3c4171752e53b24f55af7234
bamfokbq <keepthefaithkb@gmail.com>
2024-09-16 17:14:39 +0000

    added the first screen

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,15 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
+import { View, Text, TextProps, useColorScheme } from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/Colors'
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+export default function ThemedText({ style, children, ...props }: TextProps) {
+    const colorScheme = useColorScheme()
 
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
-
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+    const color = Colors[colorScheme ?? "light"].text;
 
   return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+      <Text style={[style, { color }]} {...props}>
+          {children}
+      </Text>
+  )
 }
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
